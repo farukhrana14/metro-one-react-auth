b9104a855107f94b75250bcec1abfe881cd04db2
Farukhrana14 <farukhrana14@gmail.com>
2021-05-02 22:55:12 +0600

    Private to Public

---
+++ src/components/Maps/Map.js
@@ -0,0 +1,64 @@
+import { DirectionsRenderer, DirectionsService, GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, { useState } from 'react';
+
+
+const Map = (props) => {
+    const { origin, destination } = props;
+    const [directiponRespnse, setDirectiponRespnse] = useState(null);
+    return (
+        <div>
+
+            <LoadScript
+                googleMapsApiKey="AIzaSyAfh95U6zJsgR_tmkK8ukPmfSirJgUwQdY"
+            >
+
+                <GoogleMap
+                    // required
+                    id='direction-example'
+                    // required
+                    mapContainerStyle={{
+                        height: '600px',
+                        width: '600px'
+                    }}
+                    // required
+                    zoom={10}
+                    // required
+                    center={{
+                        lat: 23.777176,
+                        lng: 90.399452
+                    }}
+                >
+
+                    <DirectionsService
+                        // required
+                        options={{
+                            destination: destination,
+                            origin: origin,
+                            travelMode: 'DRIVING'
+                        }}
+                        // required
+                        callback={res => {
+                            if (res !== null) {
+                                setDirectiponRespnse(res);
+                            }
+                        }}
+
+                    />
+
+                   
+                    {
+                        directiponRespnse && <DirectionsRenderer
+                            // required
+                            options={{ directions: directiponRespnse }}
+
+                        />
+                    }
+
+
+                </GoogleMap>
+            </LoadScript>
+        </div>
+    );
+};
+
+export default Map;
